f2fb6b3ae6e9bc07e3daebdd3dfd1bf068fc9380
bhupeshbhatia <bhupesh.bhatia@gmail.com>
2018-09-17 04:57:08 -0400

    Add Inventory form completed

--- src/app/inventory/add/add.component.ts
+++ src/app/inventory/add/add.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-declare interface User {
-  text?: string;
-  email?: string; //  must be valid email format
-  password?: string; // required, value must be equal to confirm password.
-  confirmPassword?: string; // required, value must be equal to password.
-  number?: number; // required, value must be equal to password.
-  url?: string;
-  idSource?: string;
-  idDestination?: string;
-}
+// declare interface User {
+//   barcode?: string;
+//   productName?: string;
+//   email?: string; //  must be valid email format
+//   password?: string; // required, value must be equal to confirm password.
+//   confirmPassword?: string; // required, value must be equal to password.
+//   number?: number; // required, value must be equal to password.
+//   url?: string;
+//   idSource?: string;
+//   idDestination?: string;
+// }
+
+declare var $: any;
+
+
+
 
 
 @Component({
@@ -20,41 +26,209 @@ declare interface User {
 })
 export class AddComponent implements OnInit {
 
-  public typeValidation: User;
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.typeValidation = {
-      text: '',
-      email: '',
-      idSource: '',
-      idDestination: '',
-      url: ''
+    this.form = this.formBuilder.group({
+      itemNumber: [null, [Validators.required, Validators.minLength(1)]],
+      productName: [null, Validators.required],
+      origin: [null, Validators.required],
+      arrivalDate: [null, Validators.required],
+      weight: [null, [Validators.required, Validators.minLength(1)]],
+      price: [null, [Validators.required, Validators.minLength(1)]],
+      sensorId: [null, [Validators.required, Validators.minLength(1)]],
+      location: [null, [Validators.required, Validators.minLength(1)]]
+    })
+
+
+    if ($('.datetimepicker').length !== 0) {
+      $('.datetimepicker').datetimepicker({
+        icons: {
+          time: 'fa fa-clock-o',
+          date: 'fa fa-calendar',
+          up: 'fa fa-chevron-up',
+          down: 'fa fa-chevron-down',
+          previous: 'fa fa-chevron-left',
+          next: 'fa fa-chevron-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        },
+        debug: true
+      });
     }
 
+    if ($('.timepicker').length !== 0) {
+      $('.timepicker').datetimepicker({
+        //          format: 'H:mm',    // use this format if you want the 24hours timepicker
+        format: 'h:mm A', // use this format if you want the 12hours timpiecker with AM/PM toggle
+        icons: {
+          time: 'fa fa-clock-o',
+          date: 'fa fa-calendar',
+          up: 'fa fa-chevron-up',
+          down: 'fa fa-chevron-down',
+          previous: 'fa fa-chevron-left',
+          next: 'fa fa-chevron-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        },
+        debug: true
+      });
+    }
   }
 
-  save(model: User, isValid: boolean) {
-    // call API to save customer
-    if (isValid) {
-      console.log(model, isValid);
-    }
+  isFieldValid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
   }
-  save1(model: User, isValid: boolean) {
-    // call API to save customer
-    if (isValid) {
-      console.log(model, isValid);
-    }
+
+  displayFieldCss(field: string) {
+    return {
+      'has-error': this.isFieldValid(field),
+      'has-feedback': this.isFieldValid(field)
+    };
   }
-  save2(model: User, isValid: boolean) {
-    // call API to save customer
-    if (isValid) {
-      console.log(model, isValid);
+
+  onSubmit() {
+    this.formSubmitAttempt = true;
+    if (this.form.valid) {
+      console.log('form submitted');
+      const resource = JSON.stringify(this.form.value);
+
+    console.log('Add Button clicked: ' + resource);
     }
+
+      // this.service.create(resource)
+    //   .subscribe(response => console.log(response));
+    // }
   }
-  onSubmit(value: any): void {
-    console.log(value);
+
+  reset() {
+    this.form.reset();
+    this.formSubmitAttempt = false;
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// public typeValidation: User;
+
+  // constructor() { }
+
+  // ngOnInit() {
+  //   this.typeValidation = {
+  //     barcode: '',
+  //     productName: '',
+  //     email: '',
+  //     idSource: '',
+  //     idDestination: '',
+  //     url: ''
+  //   }
+
+  // }
+
+  // save(model: User, isValid: boolean) {
+  //   // call API to save customer
+  //   if (isValid) {
+  //     console.log(model, isValid);
+  //   }
+  // }
+  // save1(model: User, isValid: boolean) {
+  //   // call API to save customer
+  //   if (isValid) {
+  //     console.log(model, isValid);
+  //   }
+  // }
+  // save2(model: User, isValid: boolean) {
+  //   // call API to save customer
+  //   if (isValid) {
+  //     console.log(model, isValid);
+  //   }
+  // }
+  // onSubmit(value: any): void {
+  //   console.log(value);
+  // }
+
+
+
+  // inventoryForm: FormGroup;
+  // itemNumber: FormControl;
+  // productName: FormControl;
+  // origin: FormControl;
+  // arrivalDate: FormControl;
+  // weight: FormControl;
+  // price: FormControl;
+  // sensorId: FormControl;
+  // location: FormControl;
+
+  // model2: Date;
+
+
+
+
+  // this.createFormControls();
+    // this.createForm();
+
+    // this.model2 = new Date();
+
+  // createFormControls() {
+  //   this.itemNumber = new FormControl(null, [
+  //     Validators.required,
+  //     Validators.minLength(1)
+  //   ]);
+
+  //   this.productName = new FormControl('', Validators.required);
+  //   this.origin = new FormControl('', Validators.required);
+  //   this.arrivalDate = new FormControl('', Validators.required);
+
+  //   this.weight = new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(1)
+  //   ]);
+
+  //   this.price = new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(1)
+  //   ]);
+
+  //   this.sensorId = new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(1)
+  //   ]);
+
+  //   this.location = new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(3)
+  //   ]);
+  // }
+
+  // createForm() {
+  //   this.inventoryForm = new FormGroup({
+  //     inventory: new FormGroup({
+  //       itemNumber: this.itemNumber,
+  //       productName: this.productName,
+  //       origin: this.origin,
+  //       arrivalDate: this.arrivalDate,
+  //       weight: this.weight,
+  //       price: this.price,
+  //       sensorId: this.sensorId,
+  //       location: this.location,
+  //     }),
+  //   });
+  // }
--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { InventoryRoutes } from './inventory.routing';
 
 import { AddComponent } from './add/add.component';
 import { ShowComponent } from './show/show.component';
+import { FieldErrorDisplayComponent } from './field-error-display/field-error-display.component';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(InventoryRoutes),
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule
     ],
     declarations: [
         AddComponent,
-        ShowComponent
+        ShowComponent,
+        FieldErrorDisplayComponent
     ]
 })
 
